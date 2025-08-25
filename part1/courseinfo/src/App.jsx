ed5eb8e1707a6b677ed4842bc7b80db6bd061d98
Gyana Sai Reddy <gjnanasai@student.nitw.ac.in>
2025-08-25 23:54:05 +0530

    1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,12 +1,19 @@
 const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.name} {props.exercises}
-      </p>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
     </>
   );
 };
@@ -16,7 +23,7 @@ const Total = (props) => {
     <p>
       Number of exercises{" "}
       {props.exercises1 + props.exercises2 + props.exercises3}
-    </p> 
+    </p>
   );
 };
 
@@ -27,14 +34,19 @@ const App = () => {
   const part2 = "Using props to pass data";
   const exercises2 = 7;
   const part3 = "State of a component";
-  const exercises3 = 14; 
+  const exercises3 = 14;
 
   return (
     <div>
       <Header course={course} />
-      <Content name={part1} exercises={exercises1} />
-      <Content name={part2} exercises={exercises2} />
-      <Content name={part3} exercises={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
